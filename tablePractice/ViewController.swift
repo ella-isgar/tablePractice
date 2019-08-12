fa36f449deb2a23b8f367263d402a834916b98cd
Ella Isgar <Ella@Apples-MacBook-Pro-63.local>
2019-08-12 14:58:36 -0400

    Completed Iteration 0

---
+++ tablePractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tablePractice
+//
+//  Created by Ella Isgar on 8/12/19.
+//  Copyright © 2019 Ella Isgar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
